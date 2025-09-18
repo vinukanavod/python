050d1cad45eb5707af15c36dba93b25815b23d06
vinukan <vinuka.navod@directfn.com>
2025-09-18 22:29:53 +0530

    create python classes

--- main.py
+++ main.py
@@ -2,12 +2,17 @@
 # • Faculty → Professor, Lecturer, TA (Teaching Assistant)
 # • Student → UndergraduateStudent, GraduateStudent
 #
-from Student import Student
+from Person import Person
+# from Student import Student
+#
+# student = Student("vinuka" , 23 , 3 )
+# student.display_info()
+# student.enroll_course("Maths")
+# student.enroll_course("Commerce")
+# student.enroll_course("Science")
+#
+# print("list of cources :" , student.courses)
 
-student = Student("vinuka" , 23 , 3 )
-student.display_info()
-student.enroll_course("Maths")
-student.enroll_course("Commerce")
-student.enroll_course("Science")
 
-print("list of cources :" , student.courses)
+person = Person("vinuka" , 23)
+person.display_info()
